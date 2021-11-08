619b54e02f41b1328cca52659b576f34dcdc4701
Irene Assinnato <irene.assinnato@mail.polimi.it>
2021-11-08 17:37:03 +0100

    Comunicazione I2C

--- GROUP_05.cydsn/Interrupt_Routines.c
+++ GROUP_05.cydsn/Interrupt_Routines.c
@@ -12,59 +12,66 @@
 #include "Interrupt_Routines.h"
 #include "project.h"
 extern uint8_t slaveBuffer[];
-int16 value_digit_TEMP;
-int16 value_digit_LDR;
-int16 value_mv_TEMP; //FLOAT???
-int16 value_mv_LDR;//FLOAT???
-int8 n_samples;
-float TEMP_avg;
-float LDR_avg;
+uint32 value_digit_TEMP;
+uint32 value_digit_LDR;
+uint16 value_mv_TEMP; //FLOAT???
+uint16 value_mv_LDR;//FLOAT???
+int8 n_samples=0;
+uint16 TEMP_avg;
+uint16 LDR_avg;
 char Transmission_Flag;
 
 
  
 CY_ISR(Custom_ISR_ADC)
 {
-    
-    if(Transmission_Flag==1){
+    /*if(Transmission_Flag==1){
        //INVIA I2C
         
         Transmission_Flag=0;
     }
+    */
     
-    else{
+    //else{
             if(n_samples<5){
                 Timer_ISR_ReadStatusRegister();
     
                 if((slaveBuffer[0] & 0b00000001)==1){
                     AMux_Select(0);
-                    value_digit_TEMP=ADC_DelSig_Read16();
-                    if (value_digit_TEMP<0)  value_digit_TEMP=0;
-                    if (value_digit_TEMP>255)  value_digit_TEMP=255;
+                    value_digit_TEMP=ADC_DelSig_Read32();
+                    //if (value_digit_TEMP<0)  value_digit_TEMP=0;
+                    //if (value_digit_TEMP>65535)  value_digit_TEMP=65535;
                     value_mv_TEMP+=ADC_DelSig_CountsTo_mVolts(value_digit_TEMP);
                     //AGGIUNGERE CONVERSIONE DA MV A R E DA R A TEMPERATURA
                 }
     
                 if((slaveBuffer[0] & 0b00000010)==2){
                     AMux_Select(1);
-                    value_digit_LDR=ADC_DelSig_Read16();
+                    value_digit_LDR=ADC_DelSig_Read32();
                     if (value_digit_LDR<0)  value_digit_LDR=0;
-                    if (value_digit_LDR>255)  value_digit_LDR=255;
+                    if (value_digit_LDR>65535)  value_digit_LDR=65535;
                     value_mv_LDR+=ADC_DelSig_CountsTo_mVolts(value_digit_LDR);
                     //AGGIUNGERE CONVERSIONE DA MV A R E DA R A INTENSITà LUCE
                 }
     
-                 n_samples++;
+                n_samples++;
                 if (n_samples==5){
                     n_samples=0;
+                    
                     TEMP_avg=value_mv_TEMP/5;
+                    slaveBuffer[3]=TEMP_avg>>8;
+                    slaveBuffer[4]=TEMP_avg & 0XFF;
+
                     LDR_avg=value_mv_LDR/5;
+                    slaveBuffer[5]=LDR_avg>>8;
+                    slaveBuffer[6]=LDR_avg & 0XFF;
+                    
                     value_mv_TEMP=0;
                     value_mv_LDR=0;
-                    Transmission_Flag=1;
+                    //Transmission_Flag=1;
                 }
             }
-        }
+        //}
 
 }
 
