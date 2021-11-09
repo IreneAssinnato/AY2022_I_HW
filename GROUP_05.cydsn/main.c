817abbbba40ae34e784ab3cd21ad06cda37b9588
Matteo <matteo2.santini@mail.polimi.it>
2021-11-09 12:17:51 +0100

    dev_main

--- GROUP_05.cydsn/main.c
+++ GROUP_05.cydsn/main.c
@@ -13,6 +13,8 @@
 #include "Interrupt_Routines.h"
 
 #define SLAVE_BUFFER_SIZE 7
+extern int8 n_samples;
+
 uint8_t slaveBuffer[SLAVE_BUFFER_SIZE]; // Buffer for the slave device
 
 int main(void)
@@ -32,19 +34,26 @@ int main(void)
     slaveBuffer[1]=0b00001010;
     
     //set WHO I AM
-    slaveBuffer[2]=0xBC;
+    slaveBuffer[2]= 0xBC;
     
     //set 3,4,5,6 slaveBuffer
-    slaveBuffer[3]=1;
-    slaveBuffer[4]=1;
-    slaveBuffer[5]=1;
-    slaveBuffer[6]=1;
+    slaveBuffer[3]=0;
+    slaveBuffer[4]=0;
+    slaveBuffer[5]=0;
+    slaveBuffer[6]=0;
     
     // Set up EZI2C buffer
     EZI2C_SetBuffer1(SLAVE_BUFFER_SIZE, 2 ,slaveBuffer);
     
     for(;;)
     {
+      /*  if(EZI2C_GetActivity()==EZI2C_STATUS_READ1 ){
+           // n_samples=0;
+           // Timer_ISR_ReadStatusRegister();
+             slaveBuffer[2]++;
+        }
+     */   
+        
         if((slaveBuffer[0] & 0b00000011)== 0b00000011){
             Pin_LED_Write(1);      
         }
