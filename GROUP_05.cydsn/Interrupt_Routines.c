8dc83ead8241bce33a80cde9f7619cb4c2b1b5c7
Matteo <matteo2.santini@mail.polimi.it>
2021-11-10 18:47:54 +0100

    develop_branch

--- GROUP_05.cydsn/Interrupt_Routines.c
+++ GROUP_05.cydsn/Interrupt_Routines.c
@@ -12,30 +12,32 @@
 #include "Interrupt_Routines.h"
 #include "project.h"
 extern uint8_t slaveBuffer[];
-uint32 value_digit_TEMP;
-uint32 value_digit_LDR;
-uint16 value_mv_TEMP;
-uint16 value_mv_LDR;
-uint16 value_TEMP;
-int8 n_samples=0;
-uint16 TEMP_avg;
-uint16 LDR_avg;
-uint16 value_TEMP_avg;
-char Transmission_Flag;
+uint32 value_digit_TEMP=0;
+uint32 value_digit_LDR=0;
+uint16 value_mv_TEMP=0;
+uint16 value_mv_LDR=0;
+uint16 value_TEMP=0;
+uint8 n_cycles=0;
+uint8 n_samples=0;
+int16 TEMP_avg=0;
+uint16 LDR_avg=0;
+
 
 
  
 CY_ISR(Custom_ISR_ADC)
 {
     Timer_ISR_ReadStatusRegister();
-                   
+           
+    
+    
     if((slaveBuffer[0] & 0b00000001)==1){
         AMux_Select(0);
         value_digit_TEMP=ADC_DelSig_Read32();
         if (value_digit_TEMP<0)  value_digit_TEMP=0;
         if (value_digit_TEMP>4294967295)  value_digit_TEMP=4294967295;
-        value_mv_TEMP+=ADC_DelSig_CountsTo_mVolts(value_digit_TEMP);
-        value_TEMP+=100*value_mv_TEMP-50;
+        value_mv_TEMP=ADC_DelSig_CountsTo_mVolts(value_digit_TEMP);
+        value_TEMP+=0.1*value_mv_TEMP-50;
     }
     
     if((slaveBuffer[0] & 0b00000010)==2){
@@ -47,26 +49,34 @@ CY_ISR(Custom_ISR_ADC)
         //AGGIUNGERE CONVERSIONE DA MV A R E DA R A INTENSITà LUCE
     }
     
-    n_samples++;
-    if (n_samples==5){
-        n_samples=0;
+    n_cycles++;
+    if (n_cycles==n_samples){
+        n_cycles=0;
             
         if((slaveBuffer[0] & 0b00000001)==1){
-            TEMP_avg=value_mv_TEMP/5;
-            value_TEMP_avg=value_TEMP/5;
+            TEMP_avg=value_TEMP/n_samples;
             slaveBuffer[3]=TEMP_avg>>8;
             slaveBuffer[4]=TEMP_avg & 0XFF;
-            value_mv_TEMP=0;
             value_TEMP=0;
         }
 
         if((slaveBuffer[0] & 0b00000010)==2){
-            LDR_avg=value_mv_LDR/5;
+            LDR_avg=value_mv_LDR/n_samples;
             slaveBuffer[5]=LDR_avg>>8;
             slaveBuffer[6]=LDR_avg & 0XFF;
             value_mv_LDR=0;
         }
+        
+        n_samples= (slaveBuffer[0] & 0b00111100)>>2;
+        Timer_ISR_WritePeriod(slaveBuffer[1]);
+        
+        if((1000/(50*n_samples)<slaveBuffer[1])){
+            Timer_ISR_WritePeriod(1000/(50*n_samples));
+        }
+       
+        
     }
+    
  }
 
 
